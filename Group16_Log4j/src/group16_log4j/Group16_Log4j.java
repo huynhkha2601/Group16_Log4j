ed615f39e182d0f15d3eb7bd2738dd3ea985991d
huynhkha2601 <63405972+huynhkha2601@users.noreply.github.com>
2021-11-16 21:20:54 +0700

    Kha first commit

---
+++ Group16_Log4j/src/group16_log4j/Group16_Log4j.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/*
+    19127055 - Lê Vũ Minh Nhật
+    19127166 - Huỳnh Tuấn Kha
+    19127565 - Nguyễn Quốc Thông
+    19127600 - Lê Quốc Trọng
+*/
+
+
+package group16_log4j;
+
+/**
+ *
+ * @author huynh
+ */
+public class Group16_Log4j {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!");
+    }
+    
+}

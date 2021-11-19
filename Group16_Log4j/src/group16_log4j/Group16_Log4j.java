6685a41f98dad699456f5077bd3a0a4c3d05cdf7
huynhkha2601 <63405972+huynhkha2601@users.noreply.github.com>
2021-11-19 11:42:00 +0700

    Kha commit
    - New proj + config log4j.properties

--- Group16_Log4j/src/group16_log4j/Group16_Log4j.java
+++ Group16_Log4j/src/group16_log4j/Group16_Log4j.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
 /*
     19127055 - Lê Vũ Minh Nhật
@@ -13,18 +9,33 @@
 
 package group16_log4j;
 
+import org.apache.log4j.Logger;
 /**
  *
  * @author huynh
  */
 public class Group16_Log4j {
 
+       // create a logger with main class
+    final static Logger logger = Logger.getLogger(Group16_Log4j.class);
+
+    // using a simple function to run some level logger with a message string
+    // whether occur if we run the proj;
+    public void getLoggerMessage(String message) {
+        logger.debug("This is DEBUG level: " + message);
+        logger.info("This is INFO level: " + message);
+        logger.warn("This is WARN level: " + message);
+        logger.error("This is ERROR level: " + message);
+        logger.fatal("This is FATAL level: " + message);
+        logger.trace("This is TRACE level: " + message);
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello world!");
+        Group16_Log4j log = new Group16_Log4j();
+        log.getLoggerMessage("Hello world!");
     }
-    
+
 }

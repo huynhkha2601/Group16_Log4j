a7c8e2c3c69e0d3a47f31b1b2952d4431823e25b
coderof19clc7 <94661526+lvmnhat19@users.noreply.github.com>
2021-11-23 18:04:06 +0700

    add 2 functions to do the "how to set Level log" part

--- Group16_Log4j/src/group16_log4j/Group16_Log4j.java
+++ Group16_Log4j/src/group16_log4j/Group16_Log4j.java
@@ -8,6 +8,7 @@
 package group16_log4j;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
 
 /**
  *
@@ -36,10 +37,34 @@ public class Group16_Log4j {
         logger.error("error log");
         logger.info("info log");
     }
-    public static void main(String[] args) {
-        Group16_Log4j log = new Group16_Log4j();
+    
+    public void check_logger_Lv(){
+        if (logger.getLevel() == null) System.out.println("NULL");
+        else System.out.println("The level of logger is: " + logger.getLevel());
+    }
+    
+    public void try_Lv_log4j(Group16_Log4j log){
+        logger.setLevel(Level.DEBUG);
+        log.check_logger_Lv();
         log.getLoggerMessage("Hello world!");
         log.DemoFile();
+        
+        logger.trace("trace: ");
+        logger.warn("warn: now we change from DEBUG to ERROR");
+        logger.fatal("fatal: ");
+        
+        logger.setLevel(Level.ERROR);
+        log.check_logger_Lv();
+        log.getLoggerMessage("Hello world!");
+        log.DemoFile();
+    }
+    
+    public static void main(String[] args) {
+        Group16_Log4j log = new Group16_Log4j();
+        //log.getLoggerMessage("Hello world!");
+        //log.DemoFile();
+        
+        log.try_Lv_log4j(log);
     }
 
 }
